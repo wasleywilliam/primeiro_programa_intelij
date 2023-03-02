bd0efcbad1ecf20be4841d20d61a1901fb236f1c
Wasley William <ww.adm@outlook.com>
2023-03-02 03:06:38 -0300

    Comit

--- primeiro_programa_intelij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro_programa_intelij/src/br/com/dio/PrimeiroPrograma.java
@@ -6,14 +6,51 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O Problema dos 3 Corpos", 300);
+        System.out.println(livro1);
 /*
     public static void main(String[] args) {
         int a =5;
         int b = 3;
         System.out.println("Hello World "+(a+b));
-
 */
+    }
+
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+
 
     }
 
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
