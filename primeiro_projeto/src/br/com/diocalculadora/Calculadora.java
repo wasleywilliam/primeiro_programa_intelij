3466217a0afdc3b70aa08bf70e1e5dd0a0936cd3
Wasley William <ww.adm@outlook.com>
2023-03-02 01:35:32 -0300

    update

---
+++ primeiro_projeto/src/br/com/diocalculadora/Calculadora.java
@@ -0,0 +1,14 @@
+package br.com.diocalculadora;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int soma (int a,int b) {
+		
+		return a+b;
+	}
+
+}
