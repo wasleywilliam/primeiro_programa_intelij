9867e96a73f66ff5c168dd1c4622090632a27428
Wasley William <ww.adm@outlook.com>
2023-03-02 02:03:40 -0300

    Update Calculadora.java

--- primeiro_projeto/src/br/com/diocalculadora/Calculadora.java
+++ primeiro_projeto/src/br/com/diocalculadora/Calculadora.java
@@ -1,14 +1,45 @@
 package br.com.diocalculadora;
 
+import java.awt.geom.FlatteningPathIterator;
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+				int a,b;
+				System.out.println("Digite o Primeiro Valor");
+				a= scan.nextInt();
+				System.out.println("Digite o Segundo Valor");
+				b= scan.nextInt();
+				
+				int soma = soma(a,b);
+				int subtracao = subtracao(a,b);
+				double divisao = divisao(a,b);
+				int multiplicacao = multiplicacao(a,b);
+				System.out.println("Soma " + soma);
+				System.out.println("Subtracao " + subtracao);
+				System.out.println("Divisao " + divisao);
+				System.out.println("Multiplicacao " + multiplicacao);
 		
 	}
 	
 	public static int soma (int a,int b) {
 		
 		return a+b;
+		
+	}
+	public static int subtracao (int a,int b) {
+		
+		return a-b;
+	}
+	public static double divisao (double a,double b) {
+		
+		return a/b;
+	}
+	public static int multiplicacao (int a,int b) {
+		
+		return a*b;
 	}
 
 }
