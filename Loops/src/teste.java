f01a0f7e2de98654bd8362929b14385155bf6e69
Wasley William <ww.adm@outlook.com>
2023-04-24 21:39:19 -0300

    update

---
+++ Loops/src/teste.java
@@ -0,0 +1,12 @@
+public class teste {
+    public static void main(String[] args) {
+        int i, num = 5;
+        for (i = 0; i < 3; i++) {
+            num += i;
+            System.out.print(i);
+        }
+
+
+    }
+
+}
